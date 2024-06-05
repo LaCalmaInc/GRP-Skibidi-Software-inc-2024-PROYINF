ef41780c1ce4625ef2b9bbb527b592c31f6bf997
soloimsad <84883623+soloimsad@users.noreply.github.com>
2024-06-05 18:25:51 -0400

    filtro aplicado

--- INF236/session/views.py
+++ INF236/session/views.py
@@ -35,49 +35,25 @@ def ver_imagenes_dicom(request):
     # Pasar la lista de archivos DICOM a la plantilla
     return render(request, 'ver_imagenes_dicom.html', {'archivos_dicom': archivos_dicom})
 
-def filtros_dicom(request):
-   
-    if request.method == 'GET':
-
-        forms= Busqueda_filtros(request.GET)
-        archivos = ArchivoDicom.objects.all()
-        
-        if forms.is_valid():
-            id_paciente= forms.cleaned_data.get('id_paciente')
-            maquinaria= forms.cleaned_data.get('maquinaria')
-            if id_paciente:
-                archivos= archivos.filter(nombre_paciente__icontains=id_paciente)
-
-            if maquinaria:
-                archivos = archivos.filter(maquinaria__icontainer=maquinaria)
-
-            return render(request,'buscar_archivo_dicom.html', {'archivos_dicom':archivos})
 
 def buscar_archivos(request):
     nombre = request.GET.get('nombre', '')
+    maquinaria = request.GET.get('maquinaria', '')
+    
     archivos = ArchivoDicom.objects.all()
-    maq= ArchivoDicom.objects.values_list("nombre_maquinaria",flat=True).distinct()
+    
     if nombre:
         archivos = archivos.filter(nombre_paciente__icontains=nombre)
+    if maquinaria:
+        archivos = archivos.filter(nombre_maquinaria__icontains=maquinaria)
+    
+    maquinarias = ArchivoDicom.objects.values_list("nombre_maquinaria", flat=True).distinct()
+    
     context = {
         'archivos': archivos,
-        'maquinarias': list(maq),
+        'maquinarias': maquinarias,
         'nombre': nombre,
+        'maquinaria_id': maquinaria,
     }
     
     return render(request, 'buscar_archivos_dicom.html', context)
-
-def buscar_maquinarias(request):
-    nombre = request.GET.get('nombre', '')
-    maquinarias = []
-    
-    if nombre:
-        archivos = ArchivoDicom.objects.filter(nombre_maquinaria__icontains=nombre)
-        maquinarias = archivos.values_list("nombre_maquinaria" , flat=True).distinct()
-    
-    context = {
-        'maquinarias': list(maquinarias),
-        'nombre': nombre,
-    }
-    
-    return render(request, 'buscar_maquinarias_dicom.html', context)
