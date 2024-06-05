0ae8cb489a41c1f070702a9ee5719179e6c518c8
CarlosVera81 <carlos.veraq@usm.cl>
2024-06-05 13:56:55 -0400

    Visualizador/index/filtros v1 up

--- INF236/session/admin.py
+++ INF236/session/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from .models import ArchivoDicom
-admin.site.register(ArchivoDicom)
-# Register your models here.
+
 from session.models import ArchivoDicom
 admin.site.register(ArchivoDicom)
